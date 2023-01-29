13c42dcaaf52177430fdbb8dac834ffbc2164475
ireongi <ireongi@gmail.com>
2023-01-29 20:39:34 +0900

    .

--- Winter_lecture/03_isPrime.py
+++ Winter_lecture/03_isPrime.py
@@ -1,13 +1,44 @@
-#소수판정 프로그램
+#prime number v0.1
+
+# number = int(input('input integer number : '))
+# counts = 0  # primenumber 판정용도
+#
+# for k in range(1, number+1): #1부터 입력된 수까지 반복
+#     if number % k == 0:
+#         counts = counts + 1
+#
+# if counts == 2:
+#     print(f'{number} is prime number!')
+# else:
+#     print(f'{number} is NOT prime number!')
+
+
+#prime number v0.2
+
+# number = int(input('input integer number : '))
+# counts = 0  # primenumber 판정용도
+#
+# for k in range(1, number+1): #2부터 입력된 수 앞까지 반복
+#     if number % k == 0:
+#         counts = counts + 1
+#
+# if counts == 0: # counts 증가하지 않았으면 소수
+#     print(f'{number} is prime number!')
+# else:
+#     print(f'{number} is NOT prime number!')
+
+
+#prime number v0.3
 
 number = int(input('input integer number : '))
-counts = 0  # primenumber 판정용도
+is_prime = True  # True가 유지되면 소수
 
-for k in range(1, number+1): #1부터 입력된 수까지 반복
+for k in range(1, number+1): #2부터 입력된 수 앞까지 반복
     if number % k == 0:
-        counts = counts + 1
+        # counts = counts + 1
+        is_prime = False
 
-if counts == 2:
+if is_prime : # is_prime 변수의 True 값이 유지되면 소수
     print(f'{number} is prime number!')
 else:
     print(f'{number} is NOT prime number!')
