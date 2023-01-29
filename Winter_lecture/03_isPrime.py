aa68a0089f1c2881b10b4744f21dee5be8a50095
ireongi <122880493+ireongi@users.noreply.github.com>
2023-01-29 20:45:45 +0900

    .

--- Winter_lecture/03_isPrime.py
+++ Winter_lecture/03_isPrime.py
@@ -42,3 +42,20 @@ if is_prime : # is_prime 변수의 True 값이 유지되면 소수
     print(f'{number} is prime number!')
 else:
     print(f'{number} is NOT prime number!')
+    
+
+    
+#prime number v0.4
+
+number = int(input('input integer number : '))
+is_prime = True  # True가 유지되면 소수
+
+for k in range(1, number+1): #2부터 입력된 수 앞까지 반복
+    if number % k == 0:
+        is_prime = False
+        break # 첫 번째 약수 발견시 for문 탈출(성능 향상)
+
+if is_prime : # is_prime 변수의 True 값이 유지되면 소수
+    print(f'{number} is prime number!')
+else:
+    print(f'{number} is NOT prime number!')    
