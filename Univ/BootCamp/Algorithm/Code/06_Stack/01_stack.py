ad46908e45aeb178fe0700ee5c9c9faad5af6521
ireongi <ireongi@gmail.com>
2023-02-06 15:01:03 +0900

    BootCamp_Algorithm_Stack

---
+++ Univ/BootCamp/Algorithm/Code/06_Stack/01_stack.py
@@ -0,0 +1,26 @@
+stack = [None, None, None, None, None]
+#from inspect import stack
+#stack[None for _ in range(5)]
+top = -1
+
+top = top + 1
+stack[top] =  "커피" # 0번방
+top = top + 1
+stack[top] = "녹차"
+top = top + 1
+stack[top] = "꿀물"
+top = top + 1
+stack[top] = "오렌지주스"
+top = top + 1
+stack[top] = "바닐라라떼"
+
+for i in range(len(stack)-1, -1, -1):
+    print(stack[i])
+
+data = stack[top]
+stack[top] = None
+top = top -1
+print(data)
+
+for i in range(len(stack)-1, -1, -1):
+    print(stack[i])
