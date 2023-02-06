521447ddd29eb3cf07f817294c746dbc4ede2d1a
ireongi <ireongi@gmail.com>
2023-02-06 15:40:31 +0900

    BootCamp_Algorithm_Stack

---
+++ Univ/BootCamp/Algorithm/Code/06_Stack/02_stack2.py
@@ -0,0 +1,68 @@
+def is_stack_full():
+    global SIZE, stack, top
+    if top >= SIZE-1:
+        return True
+    return False
+
+
+def is_stack_empty():
+    global SIZE, stack, top
+    if top == -1:   # top이 없으면
+        return True
+    return False
+
+
+def push(data):
+    global SIZE, stack, top
+    if is_stack_full():
+        print("Stack is FULL!")
+        return
+    top = top + 1
+    stack[top] = data
+
+
+def pop():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is EMPTY~")
+        return
+    temp = stack[top]
+    stack[top] = None
+    top = top - 1
+    return temp
+
+
+def peek():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is EMPTY~")
+        return
+    return stack[top]
+
+
+SIZE = int(input("Stack SIZE : "))
+stack = [None for _ in range(SIZE)]
+top = -1
+
+
+if __name__ == "__main__":
+    while True:
+        menu = input("Insert(I)/Extrack(E)/Verify(V)/eXit(X) 중 하나를 선택 ==> ")
+        if menu == 'X' or menu == 'x':
+            break
+        elif menu == 'I' or menu == 'i':
+            data = input("Input Data ==> ")
+            push(data)
+            print("Stack Status : ", stack)
+        elif menu == 'E' or menu == 'e':
+            data = pop()
+            print("Input Data ==> ", data)
+            print("Stack Status : ", stack)
+        elif menu == 'V' or menu == 'v':
+            data = peek()
+            print("Check Data ==> ", data)
+            print("Stack Status : ", stack)
+        else:
+            print("Input mismatch")
+    print("Program End")
+
